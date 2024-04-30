f38de762956b3dba4a7abd8c3666a3a4a4b59cce
SameerVerma-debug <129542219+SameerVerma-debug@users.noreply.github.com>
2024-04-30 16:58:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ routes/api.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import auth from "./auth.js";
+import location from "./location.js";
+import profile from "./profile.js";
+import allocateCaretaker from "../controller/caretakerAlloc/caretakerAllocation.js";
+import assignedPatients from "../controller/caretakerAlloc/assignedPatients.js";
+import forgotPassword from "./forgotPassword.js";
+const router = express.Router();
+
+router.use("/auth", auth);
+router.use("/profile", profile);
+router.use("/location", location);
+router.use("/forgotpassword",forgotPassword);
+router.post("/allocateCaretaker", allocateCaretaker);
+router.get("/patientAssigned", assignedPatients);
+
+export default router;
